26d39a3aed8720ed23c0476923bd8e3f0b2516b2
vcico <cgjcgs@gmail.com>
2017-12-29 19:40:40 +0800

    切换房间

--- Applications/Chat/Behavior/ChangeroomBehavior.php
+++ Applications/Chat/Behavior/ChangeroomBehavior.php
@@ -2,16 +2,33 @@
 
 namespace Chat\Behavior;
 
+use Chat\Lib\User;
+use Container;
+use GatewayWorker\Lib\Gateway;
 /**
  * 切换房间
+ * 1 数据验证失败
  */
 
 class  ChangeroomBehavior extends BaseBehavior
 {
 
+    public function rules() {
+        return [
+            ['room_id','required'],
+            ['room_id','integer'],
+        ];
+    }    
+    
     public function run($client_id,$message)
     {
-        
+        $data = $message['data'];
+        if(!Container::$validator->validate($this->rules(), $data))
+        {
+            Gateway::sendToCurrentClient(Container::encodeMessage($message['type'],[],1,'房间ID不合法'));
+        }
+        User::ChangeRoom($client_id, $data['room_id']);
+        Gateway::sendToCurrentClient(Container::encodeMessage($message['type'],[]));
     }
 
 
