16a02be6c225546ffe23e1af58d40af0be587ffb
vcico <cgjcgs@gmail.com>
2018-01-12 14:31:19 +0800

    视频jsonp添加回调函数

--- Applications/Chat/Web/video.php
+++ Applications/Chat/Web/video.php
@@ -5,7 +5,7 @@ try{
         foreach($result as $key => $val){
             $result[$key]['file'] = 'http://dev.sex.com/'.$val['file'];
         }
-	echo json_encode(['errCode'=>'0','info'=>'','data'=>$result]);
+	echo 'callback('.json_encode(['errCode'=>'0','info'=>'','data'=>$result]).')';
 }catch(SOAPFault $e){
-	echo json_encode(['errCode'=>'1','info'=>'数据请求失败','data'=>$e->getMessage()]);
+	echo 'callback('. json_encode(['errCode'=>'1','info'=>'数据请求失败','data'=>$e->getMessage()]).')';
 }
